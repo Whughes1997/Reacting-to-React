878c9a94fefefb8913ee1bc5a87e307f8720935e
Will Hughes <killjoyfpss@wills-mbp.lan>
2022-03-16 09:42:51 -0500

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Greeter from './Components./Greeter'
+
+const App = () => {
+    return (
+        <>
+        <h1>yeeet</h1>
+        <Greeter />
+        <Greeter />
+        <Greeter />
+        <input type="text" />
+        <p> `You are logging in as ${username}` </p>
+        let username = useState
+        let loaded = useState = () =>{
+            if (false) {
+                return (
+                <h1> Website loading...</h1>
+                )
+                else {
+                    return (
+                        useEffect(() => {
+                            document.title = `You clicked ${count} times`;
+                          });
+                    )
+                }
+                    
+                }
+            }
+        }
+        </>
+
+    )
+}
+
+export default App;
+import 
---
+++ src/Components/Greeter.jsx
@@ -0,0 +1,8 @@
+let phrase = 'Hello there,'
+let name = 'General Kenobi'
+const App = (Phrase, Name) => {
+    return (
+        `${phrase}, ${name}`
+    )
+}
+export default App
